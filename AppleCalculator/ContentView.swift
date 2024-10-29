43bbe90ccd611192ca3bcf2b5d6640cd7aa586e1
Sarah Roy <sarah2000roy@gmail.com>
2024-10-29 19:27:22 -0400

    added stack to hold text + buttons

--- AppleCalculator/ContentView.swift
+++ AppleCalculator/ContentView.swift
@@ -7,15 +7,34 @@
 
 import SwiftUI
 
+enum CalculatorButton: String, Identifiable {
+    case zero, one, two, three, four, five, six, seven, eight, nine, plus, minus, multiply, divide, equal, clear, decimal, percent, sign
+    
+    var id: Self { self }
+}
 struct ContentView: View {
+    //2D array for calc button grid
+    let buttons: [[CalculatorButton]] = []
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, sarah!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all) //black bg takes up all the space (corners)
+            
+            VStack { //vertical stack
+                
+                //TEXT DISPLAY
+                HStack { //horizotal stack - to align to the right
+                    Spacer() //push text to the rightmost end
+                    Text("0") //placeholder text
+                        .bold() //make text bold
+                        .font(.system(size: 64)) //font size
+                        .foregroundColor(.white)//font colour
+                }
+                .padding() //padding on the left of the placeholder
+                
+                //BUTTONS
+                
+            }
         }
-        .padding()
     }
 }
 
