363df94766a70535adb8c4322c185c5fabf272cf
Sarah Roy <sarah2000roy@gmail.com>
2024-10-29 18:20:41 -0400

    Initial Commit

---
+++ AppleCalculator/AppleCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleCalculatorApp.swift
+//  AppleCalculator
+//
+//  Created by Sarah Roy on 2024-10-29.
+//
+
+import SwiftUI
+
+@main
+struct AppleCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleCalculator
+//
+//  Created by Sarah Roy on 2024-10-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
